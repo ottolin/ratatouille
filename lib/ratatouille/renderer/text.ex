9119ccc46caeb9c4484dff8e11775f5237d920b0
otto.lin <otto.lin@tfidm.com>
2019-06-20 11:24:19 +0800

    Fixing multibyte unicode width rendering

--- lib/ratatouille/renderer/text.ex
+++ lib/ratatouille/renderer/text.ex
@@ -14,10 +14,44 @@ defmodule Ratatouille.Renderer.Text do
     cells =
       text
       |> String.graphemes()
-      |> Enum.with_index()
+      #|> Enum.with_index()
+      |> process_text
       |> Enum.map(cell_generator)
 
     Canvas.merge_cells(canvas, cells)
+  end
+
+  def string_width(str) do
+    str |> String.graphemes |> Enum.map(&grapheme_width/1) |> Enum.sum
+  end
+
+  def grapheme_width(<<g::utf8>>) do
+    if g >= 0x1100 &&
+       (g <= 0x115f || g == 0x2329 || g == 0x232a ||
+            (g >= 0x2e80 && g <= 0xa4cf && g != 0x303f) ||
+            (g >= 0xac00 && g <= 0xd7a3) ||
+            (g >= 0xf900 && g <= 0xfaff) ||
+            (g >= 0xfe30 && g <= 0xfe6f) ||
+            (g >= 0xff00 && g <= 0xff60) ||
+            (g >= 0xffe0 && g <= 0xffe6) ||
+            (g >= 0x20000 && g <= 0x2fffd) ||
+            (g >= 0x30000 && g <= 0x3fffd)) do
+      2
+    else
+      1
+    end
+  end
+
+  def process_text(graphemes) do
+    {rv, _} =
+      graphemes
+      |> Enum.reduce({[], 0},
+           fn g, {g_list, offset} ->
+             #ch_len = if :unicode.bin_is_7bit(g), do: 1, else: 2
+             ch_len = g |> grapheme_width
+             {[{g, offset}|g_list], offset + ch_len}
+           end)
+    rv |> Enum.reverse
   end
 
   def render_group(canvas, text_elements, attrs \\ %{}) do
@@ -38,7 +72,7 @@ defmodule Ratatouille.Renderer.Text do
 
     canvas
     |> render(canvas.render_box.top_left, text, attrs)
-    |> Canvas.translate(String.length(text), 0)
+    |> Canvas.translate(string_width(text), 0)
   end
 
   defp template_cell(attrs) do
